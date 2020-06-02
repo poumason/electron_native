38dd79cb546fb91990434d6d5d58e2f7e573ae55
Pou Lin <poumason@live.com>
2020-06-03 00:59:43 +0800

    add c# projects

---
+++ TestDll/TestDll.UWP/MyAudioPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+
+namespace TestDll.UWP
+{
+    public class MyAudioPlayer
+    {
+        private MediaPlayer player;
+
+        public MyAudioPlayer()
+        {
+            player = new MediaPlayer();
+            player.AutoPlay = true;
+        }
+
+        public async Task<object> Play(object param)
+        {
+            player.Source = MediaSource.CreateFromUri(new Uri("https://pebblebus.blob.core.windows.net/pebble/love.mp3"));
+            player.Play();
+            return "UWP";
+        }
+
+        public async Task Stop()
+        {
+            player.Pause();
+        }
+    }
+}
